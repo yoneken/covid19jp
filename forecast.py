055894ce3d810e751c09f0296ba16de9a29cbbd6
Kenta Yonekura(a.k.a. yoneken) <yoneken@ieee.org>
2020-04-13 09:37:29 +0900

    Update 20/04/13

--- forecast.py
+++ forecast.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
 def gen_eq_str(coefficients:list, x:str="x", y:str="y"):
     s = ""
